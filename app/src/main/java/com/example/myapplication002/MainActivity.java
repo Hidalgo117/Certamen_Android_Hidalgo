b7b4f0b0647cd740d378967f7c5c2dcf63fcacbb
hidalgo117 <sergox377@gmail.com>
2024-11-12 14:43:55 -0300

    Codigo

--- app/src/main/java/com/example/myapplication002/MainActivity.java
+++ app/src/main/java/com/example/myapplication002/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.myapplication002;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText peso,estatura;
+    Spinner sexo;
+    TextView IMC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sexo = findViewById(R.id.spr_Sexo);
+        IMC = findViewById(R.id.Txt_Mostrar);
+        peso=findViewById(R.id.TxN_Peso);
+        estatura = findViewById(R.id.Txn_Estatura);
+
+        
+
+
     }
 }
